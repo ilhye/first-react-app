5f40f41562ece67b411b9d9deff5cf798edb5deb
ilhye <144883820+ilhye@users.noreply.github.com>
2024-04-20 13:35:12 +0800

    update ListGroup

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,9 +1,9 @@
 export default function ListGroup() {
   // enclosed with curly braces to turn them into objects
   const items = [
-    { id: 1, location: "San Jose", country: "Philippines" },
-    { id: 2, location: "New York", country: "United States" },
-    { id: 3, location: "Taiwan", country: "China" },
+    { id: 1, location: "San Jose", zipCode: 3023, country: "Philippines" },
+    { id: 2, location: "New York", zipCode: 11368, country: "United States" },
+    { id: 3, location: "Taiwan", zipCode: 106216, country: "China" },
   ]; 
 
   /**
@@ -14,9 +14,12 @@ export default function ListGroup() {
    * Sort using COUNTRY
    * items.sort((a, b) => a.country.localeCompare(b.country)); - Alphabetical
    * items.sort((a, b) => b.country.localeCompare(a.country)); - Reverse
+   * 
+   * Sort using NUMBER
+   * items.sort((a, b) => a.zipCode - b.zipCode) - NUMERIC
+   * items.sort((a, b) => b.zipCode - a.zipCode) - REVERSE NUMERIC
    */
 
-  
   /**
    * Key - unique identifier
    * essential siya kapag magc-change, add, or delete
@@ -24,7 +27,7 @@ export default function ListGroup() {
    */
   const listItems = items.map((items) => (
     <li key={items.id}>
-      {items.location}, {items.country}
+      {items.location}, {items.country}, {items.zipCode}
     </li>
   ));
 
